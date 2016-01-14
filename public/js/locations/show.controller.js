e9c48f62493b8a7bf09f15edccb331445636d011
Britton Walker <bwalker1801@gmail.com>
2016-01-14 15:04:38 -0500

    ability to post to location and show all posts on location show page

--- public/js/locations/show.controller.js
+++ public/js/locations/show.controller.js
@@ -22,6 +22,27 @@
         .then(function(result) {
           vm.myData = result.data;
          });
-         
+
+    $scope.formData = {};
+
+    $scope.processForm = function() {
+      $scope.url2 = 'http://localhost:3000/api/locations/' + id + '/posts';
+
+      $http({
+      method    : 'POST',
+      url       : $scope.url2,
+      data      : $.param($scope.formData),
+      headers   : { 'Content-Type': 'application/x-www-form-urlencoded'}
+      })
+        .success(function(data) {
+          console.log(data);
+
+          if (!data.success) {
+            $scope.errorName = data.errors.name
+          } else {
+            $scope.message = data.message;
+          }
+        })
+    };
   }
 }());
